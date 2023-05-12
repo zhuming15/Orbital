044db208d60d8644698d42b7cf3b0fa298a1578a
zhuming15 <e0735464@u.nus.edu>
2023-05-12 17:32:13 +0800

    Add setFormMessage and setInputError method
    	- setFormMessage: show red error / green success message
    	- setInputError: red highlight if input is invalid, else
    	  green

--- draft/scr/main.js
+++ draft/scr/main.js
@@ -1,3 +1,16 @@
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
