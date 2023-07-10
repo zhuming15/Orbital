366e59684bb5ff0891670f5536e41e1c117e435b
Shao Wei <swtan346@gmail.com>
2023-07-10 14:22:26 +0800

    fixed typo

--- project/my-project-name/backend/components/login.js
+++ project/my-project-name/backend/components/login.js
@@ -15,10 +15,10 @@ app.post('/api/login/:email/:password', (req, res) => {
   const password = req.params.password;
   const query = `SELECT * FROM users WHERE email = ?`;
 
-  planetscale.query(query, email, (err, result) => {
+  planetscale.query(query, [email], (err, result) => {
     if (!result || result.length === 0) {
       return res.status(404).json({ error: 'Invalid email or password' });
-    } else if (result[0].password_hash !== password) {
+    } else if (result[0].password !== password) {
       return res.status(401).json({ error: 'Invalid email or password' });
     }
     return res.status(200).json({ message: 'Login successful' });
